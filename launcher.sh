dd52ee2e36014cf9a0bf1f4ef426e44028abc402
AngryBananer <15734119+AngryBananer@users.noreply.github.com>
2019-12-20 22:19:18 +0100

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -2,7 +2,6 @@
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
 
-cd /
-cd home/pi/CoPilot
-sudo python2.7 main.py
-cd /
+cd /home/pi/CoPilot
+python2.7 main.py
+cd
